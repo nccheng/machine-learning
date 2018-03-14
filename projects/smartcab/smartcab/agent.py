f7670fb33d316e5780935f6d8936497c0289341e
nccheng <naichia741224@gmail.com>
2018-03-14 13:51:57 +0000

    Completed Q5 & Q-Learning

--- projects/smartcab/smartcab/agent.py
+++ projects/smartcab/smartcab/agent.py
@@ -30,6 +30,8 @@ class LearningAgent(Agent):
         ## TO DO ##
         ###########
         # Set any additional class parameters as needed
+	
+        self.trail = 0
 
 
     def reset(self, destination=None, testing=False):
@@ -46,7 +48,14 @@ class LearningAgent(Agent):
         # Update epsilon using a decay function of your choice
         # Update additional class parameters as needed
         # If 'testing' is True, set epsilon and alpha to 0
-
+        if testing:
+			     self.epsilon = 0.0
+			     self.alpha = 0.0
+        else:
+            self.epsilon = self.epsilon - 0.05
+            #self.epsilon = 0.99 ** self.trail
+            self.trail += 1
+                 
         return None
 
     def build_state(self):
@@ -57,7 +66,7 @@ class LearningAgent(Agent):
         # Collect data about the environment
         waypoint = self.planner.next_waypoint() # The next waypoint 
         inputs = self.env.sense(self)           # Visual input - intersection light and traffic
-        deadline = self.env.get_deadline(self)  # Remaining deadline
+        #deadline = self.env.get_deadline(self)  # Remaining deadline
 
         ########### 
         ## TO DO ##
@@ -70,7 +79,7 @@ class LearningAgent(Agent):
         
         # Set 'state' as a tuple of relevant data for the agent
 		
-        import pdb; pdb.set_trace()
+        #import pdb; pdb.set_trace()
 		
         state = (waypoint, inputs['light'], inputs['oncoming'], inputs['left'])
 
@@ -86,7 +95,7 @@ class LearningAgent(Agent):
         ###########
         # Calculate the maximum Q-value of all actions for a given state
 
-        maxQ = None
+        maxQ = max(self.Q[state].values())
 
         return maxQ 
 
@@ -100,7 +109,12 @@ class LearningAgent(Agent):
         # When learning, check if the 'state' is not in the Q-table
         # If it is not, create a new dictionary for that state
         #   Then, for each action available, set the initial Q-value to 0.0
-
+		
+        if self.learning == True:
+            if state not in self.Q.keys():
+                self.Q[state] = {}
+                for act in self.valid_actions:
+                    self.Q[state][act] = 0.0 
         return
 
 
@@ -143,6 +157,8 @@ class LearningAgent(Agent):
         ###########
         # When learning, implement the value iteration update rule
         #   Use only the learning rate 'alpha' (do not use the discount factor 'gamma')
+        if self.learning == True:
+            self.Q[state][action] = (1 - self.alpha)*self.Q[state][action] + self.alpha*reward
 
         return
 
@@ -179,7 +195,7 @@ def run():
     #   learning   - set to True to force the driving agent to use Q-learning
     #    * epsilon - continuous value for the exploration factor, default is 1
     #    * alpha   - continuous value for the learning rate, default is 0.5
-    agent = env.create_agent(LearningAgent)
+    agent = env.create_agent(LearningAgent, learning=True)
     
     ##############
     # Follow the driving agent
