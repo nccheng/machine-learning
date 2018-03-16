a87da6b8a8a5b9cfcb76d998616ffa93d3e60668
nccheng <naichia741224@gmail.com>
2018-03-16 23:44:08 +0800

    Completed Q6

--- projects/smartcab/smartcab/agent.py
+++ projects/smartcab/smartcab/agent.py
@@ -52,9 +52,12 @@ class LearningAgent(Agent):
 			     self.epsilon = 0.0
 			     self.alpha = 0.0
         else:
-            self.epsilon = self.epsilon - 0.05
-            #self.epsilon = 0.99 ** self.trail
             self.trail += 1
+            #self.epsilon = self.epsilon - 0.05
+            self.epsilon = 0.99 ** self.trail
+            #self.epsilon = 1.0 / (self.trail ** 2)
+            #self.epsilon = math.exp(-1 * 0.99 * self.trail)
+            #self.epsilon = math.fabs(math.cos(0.01 * self.trail))
                  
         return None
 
@@ -210,14 +213,14 @@ def run():
     #   display      - set to False to disable the GUI if PyGame is enabled
     #   log_metrics  - set to True to log trial and simulation results to /logs
     #   optimized    - set to True to change the default log file name
-    sim = Simulator(env, update_delay=0.01, log_metrics=True)
+    sim = Simulator(env, update_delay=0.01, log_metrics=True, optimized=True)
     
     ##############
     # Run the simulator
     # Flags:
     #   tolerance  - epsilon tolerance before beginning testing, default is 0.05 
     #   n_test     - discrete number of testing trials to perform, default is 0
-    sim.run(n_test=10)
+    sim.run(n_test=100, tolerance=0.001)
 
 
 if __name__ == '__main__':
